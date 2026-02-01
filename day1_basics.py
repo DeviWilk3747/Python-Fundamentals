a7dedc9d830ed4f3c723f0cfa06010178e676dcf
DeviWilk <dwilkerson@stu.jsu.edu>
2026-01-31 20:36:47 -0600

    Day 1 - variables and input practice

---
+++ day1_basics.py
@@ -0,0 +1,17 @@
+print("Welcome to Python")
+
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+
+print(f"Hello {name}! You're favorite color is {color}.")
+
+# Asking for two numbers and getting the sum of them
+a = int(input("Give me a random number: "))
+b = int(input("Give me another number: "))
+sum = a + b
+print(f"The sum of the two number you gave me are: {sum}. ")
+
+# Asking for city and major
+city = input("What city do you live in? ")
+major = input("What are you currently majoring in? ")
+print(f"You currently reside in the city of {city} and is currently majoring in {major}.")

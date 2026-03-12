0605bb9abea52c1c096a6767af141b3ce2e49f5c
DeviWilk <dwilkerson@stu.jsu.edu>
2026-03-12 04:41:39 -0500

    Create Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,16 @@
+# Create dictionary
+student = {
+    "name": "Devian Wilkerson",
+    "major": "Computer Information Systems",
+    "school": "Jacksonville State University",
+    "graduation year": "2026"
+}
+print("Student Name:", student["name"])
+print("Major:", student["major"])
+print("School:", student["school"])
+
+student["major"] = "Software Engineering"
+print(student["major"])
+
+student["fav_language"] = "Python"
+print(student["fav_language"])

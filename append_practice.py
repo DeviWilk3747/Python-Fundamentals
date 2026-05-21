bbeef716d1689029c6fad7017a81f7010e6ab4e7
DeviWilk3747 <devi.wilkerson@gmail.com>
2026-05-21 15:56:34 -0500

    Commit

--- append_practice.py
+++ append_practice.py
@@ -1,3 +1,8 @@
+# Open the file in append mode so new data is added
 file = open("students.txt", "a")
+
+# Writes student information to the file
 file.write("Devian, CIS, 2026\n")
+
+# Closes the file and saves changes
 file.close()
--- file_practice.py
+++ file_practice.py
@@ -1,3 +1,4 @@
+# Creates a file and writes a message in it
 file = open("message.txt", "w")
 file.write("Hello Devian")
 file.close()
